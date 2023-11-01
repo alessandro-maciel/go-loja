25cfdc45be431402dddcc91c2d660b7c784230f1
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-11-01 15:22:50 -0300

    feat: creating server and product struct

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{Nome: "Tenis", Descricao: "Confortável", Preco: 99.99, Quantidade: 3},
+		{Nome: "Fone", Descricao: "Muito bom", Preco: 959, Quantidade: 2},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
+}
